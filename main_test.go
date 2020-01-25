74cda5d7bd3d274c04c9f6d71ee374eab1f7d430
Shuhrat.Shokirov <avatar_9999@list.ru>
2020-01-25 15:14:04 +0300

    correct Test

--- main_test.go
+++ main_test.go
@@ -4,31 +4,25 @@ import "fmt"
 
 var data = []account{
 	{id: 1, name: "Misha", balance: 10_000},
-	{id: 2, name: "Masha", balance: 14_000},
-	{id: 3, name: "Alex", balance: 8_000},
-	{id: 4, name: "Erzhan", balance: 15_000},
-	{id: 5, name: "Sasha", balance: 12_000},
-	{id: 6, name: "Nastya", balance: 11_000},
-	{id: 7, name: "Petya", balance: 9_000},
-	{id: 8, name: "Natasha", balance: 7_000},
+	{id: 2, name: "Masha", balance: 15_000},
 }
 
 func Example_FunctionAllForTrue() {
 	fmt.Println(all(data, func(item account) bool {
-		return item.balance >= 7_000
+		return item.balance >= 10_000
 	}))
 	// Output: true
 }
 func Example_FunctionAllForFalse() {
 	fmt.Println(all(data, func(item account) bool {
-		return item.balance >= 7_500
+		return item.balance > 10_000
 	}))
 	// Output: false
 }
 
 func Example_FunctionAnyForTrue() {
 	fmt.Println(any(data, func(item account) bool {
-		return item.balance >= 7_500
+		return item.balance > 10_000
 	}))
 	// Output: true
 }
@@ -41,13 +35,13 @@ func Example_FunctionAnyForFalse() {
 
 func Example_FunctionNoneForTrue() {
 	fmt.Println(none(data, func(item account) bool {
-		return item.balance < 7_000
+		return item.balance < 10_000
 	}))
 	// Output: true
 }
 func Example_FunctionNoneFalse() {
 	fmt.Println(none(data, func(item account) bool {
-		return item.balance > 7_000
+		return item.balance <= 10_000
 	}))
 	// Output: false
 }
@@ -60,26 +54,26 @@ func Example_IndexHasResult() {
 }
 func Example_IndexHasNotResult() {
 	fmt.Println(index(data, func(item account) bool {
-		return item.balance <= 3_000
+		return item.balance > 15_000
 	}))
 	// Output: -1
 }
 
 func Example_FindHasResult() {
 	result, ok := find(data, func(item account) bool {
-		return item.name == "Alex"
+		return item.name == "Masha"
 	})
 	if ok != nil {
 		fmt.Errorf(ok.Error())
 	} else {
 		fmt.Println(result)
 	}
-	// Output: {3 Alex 8000}
+	// Output: {2 Masha 15000}
 }
 
 func Example_FindWithError() {
 	result, ok := find(data, func(item account) bool {
-		return item.balance < 3_000
+		return item.name == "Alex"
 	})
 	if ok != nil {
 		fmt.Println(ok.Error())
